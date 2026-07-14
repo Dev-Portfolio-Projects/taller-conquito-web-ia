fab08802254bd1a671db66126bb5b05d3f029248
Salinas Cruz Edison <salinasedisoncruz@gmail.com>
2026-07-14 15:25:57 -0500

    chore: inicializar proyecto

---
+++ proyecto-final/src/main.js
@@ -0,0 +1,11 @@
+import './styles/main.css';
+
+const app = document.querySelector('#app');
+
+app.innerHTML = `
+  <main class="min-h-screen flex items-center justify-center bg-stone-100">
+    <h1 class="text-4xl font-bold text-amber-900">
+      Quito Coffee Roasters
+    </h1>
+  </main>
+`;
